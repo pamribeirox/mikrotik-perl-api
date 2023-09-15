4b5d0d7f3cdd70b352403f53a46b7e2826aac2dc
Pedro Ribeiro <pamribeirox+buhtig@gmail.com>
2023-09-15 10:12:16 +0100

    Changed Mtik.pm to allow TLS/SSL API access
    Changed Mtik.pm to allow TLS/SSL API access
    Work done by the DSIC/IPL team (Vasco Mendonça)

--- Mtik.pm
+++ Mtik.pm
@@ -1,15 +1,18 @@
 #! /usr/bin/perl -w
 # Mtik.pm - a simple Mikrotik Router API client
-# Version 1.0 Beta
+# Version 1.2 Beta
 # Hugh Messenger - hugh at alaweb dot com
 # Released under Creative Commons license.
 # Do with it what you will, but don't blame me!
 #----------------
 
 package Mtik;
-$VERSION = '0.01';
+$VERSION = '0.02';
 $debug = 0;
 $error_msg = '';
+
+our($ssloff);
+our($sslvm);
 
 use strict;
 use vars qw(
@@ -22,7 +25,10 @@ use vars qw(
            );
 
 use IO::Socket;
+use IO::Socket::SSL qw(debug0);
 use Digest::MD5;
+use Data::Dumper;
+
 
 @ISA        = qw(Exporter);
 @EXPORT     = qw();
@@ -36,19 +42,42 @@ my($sock);
 sub mtik_connect
 {
     my($host) = shift;
-    my($port) = shift || 8728;
+    my($port) = shift || 8729;
     if (!($host))
     {
         print "no host!\n";
         return 0;
     }
-    my($sock) = new IO::Socket::INET(
-                    PeerAddr => $host,
-                    PeerPort => $port,
-                    Proto    => 'tcp');
+	my($sock);
+	if(defined $ssloff)
+	{
+		#SSL off (use flag -s)
+		$sock = new IO::Socket::INET(
+						PeerAddr => $host,
+						PeerPort => $port,
+						Proto    => 'tcp');
+	}
+	else
+	{
+		#print "$sslvm\n";
+		#SSL on (Default)
+		$sock = new IO::Socket::SSL(
+						PeerAddr => $host,
+						PeerPort => $port,
+						SSL_verify_mode => $sslvm,#; #$sslvm
+						SSL_ca_path => '/etc/ssl/certs',
+						Timeout => 5);
+						
+		#SSL_verify_mode. 0x00 (default, does no authentication),
+		#				  0x01 (verify peer),
+		#				  0x02 (fail verify if no peer cert exists; ignored for clients),
+		#			  	  0x04 (verify client once).
+		#SSL_verifycn_name
+	}
     if (!($sock))
     {
-        print "no socket :$!\n";
+        #print "no socket :$!\n";
+	print "No socket ($host:$port) - $!\n";
         return 0;
     }
     return $sock;
@@ -113,7 +142,16 @@ sub write_len {
 
 sub read_byte{
 	my $line;
-	$sock->recv($line,1);
+	if(defined $ssloff)
+	{
+		#SSL off
+		$sock->recv($line,1);
+	}
+	else
+	{
+		#SSL on
+		sysread($sock,$line,1);
+	}
 	return ord($line);
 }
 
@@ -181,7 +219,16 @@ sub read_word {
         }
         while (1) {
             my($line) = '';
-            $sock->recv($line,$len);
+			#if because of ssl on or off
+			if(defined $ssloff)
+			{
+				#SSL off
+				$sock->recv($line,$len);
+			}
+			else{
+				#SSL on
+				sysread($sock,$line,$len);
+			}
             # append to $ret_line, in case we didn't get the whole word and are going round again
             $ret_line .= $line;
             my $got_len = length($line);
@@ -287,16 +334,41 @@ sub login
     my($host) = shift;
     my($username) = shift;
     my($passwd) = shift;
-
-    if (!($sock = &mtik_connect($host)))
+	my($port) = shift;
+	my($loginMode) = shift;
+	$ssloff = shift;
+	$sslvm = shift;# || default 0x01;
+	
+    if (!($sock = &mtik_connect($host,$port)))
     {
         return 0;
     }
     my(@command);
     my($retval,@results);
-    push(@command, '/login');
-    push(@command, '=name=' . $username);
-    push(@command, '=password=' . $passwd);
+	
+	if(defined $loginMode)
+	{
+		#Login method pre-v6.43
+		push(@command,'/login');
+		my($retval,@results) = talk(\@command);
+		my($chal) = pack("H*",$results[0]{'ret'});
+		my($md) = new Digest::MD5;
+		$md->add(chr(0));
+		$md->add($passwd);
+		$md->add($chal);
+		my($hexdigest) = $md->hexdigest;
+		undef(@command);
+		push(@command, '=name=' . $username);
+		push(@command, '=response=00' . $hexdigest);
+	}
+	else
+	{
+		#Login method post-v6.43
+		push(@command, '/login');
+		push(@command, '=name=' . $username);
+		push(@command, '=password=' . $passwd);
+	}
+	
     ($retval,@results) = &talk(\@command);
     if ($retval > 1)
     {
@@ -312,7 +384,19 @@ sub login
 
 sub logout
 {
-    close $sock;     
+	if(defined $ssloff)
+	{
+		#SSL off
+		close $sock;
+	}
+	else
+	{
+		#SSL on
+		$sock->close(
+		SSL_no_shutdown => 1, 
+		SSL_ctx_free    => 1,    
+		)
+	}
 }
 
 sub get_by_key
@@ -321,14 +405,16 @@ sub get_by_key
     my($id) = shift || '.id';
     $error_msg = '';
     my(@command);
-    push(@command,$cmd);
+    @command = split / /, $cmd;
     my(%ids);
+    my $count = 0;
     my($retval,@results) = &Mtik::talk(\@command);
     if ($retval > 1)
     {
         $error_msg = $results[0]{'message'};
         return %ids;
     }
+    #print "\n" . Dumper(\@results) ."\n";
     foreach my $attrs (@results)
     {
         my $key = '';
@@ -345,7 +431,11 @@ sub get_by_key
         {
             $ids{$key} = $attrs;
         }
+	else {
+		$ids{("n" . $count)} = $attrs;
+	}
     }
+    #print "\n" . Dumper(\%ids) ."\n";
     return %ids;
 }
 
